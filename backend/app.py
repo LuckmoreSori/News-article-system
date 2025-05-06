68f365dbd22831612da9fed87328e24e93b81081
LuckmoreSori <luckmoretaedza@gmail.com>
2025-05-06 18:10:01 +0200

    changes in app run

--- backend/app.py
+++ backend/app.py
@@ -28,4 +28,5 @@ def get_articles():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
+
